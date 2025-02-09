5c8f84425af840787ca7b55f59952eeda82426c6
Ram <113391531+RamanagowdaMPateela@users.noreply.github.com>
2025-02-09 18:50:31 +0530

    firstCommit (#1)
    * firstCommit
    
    * firstCommit

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+ // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
@@ -35,7 +35,7 @@ export default defineConfig({
   use: {
 
     browserName: 'chromium',
-    headless:false,
+    
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://rahulshettyacademy.com/',
 
